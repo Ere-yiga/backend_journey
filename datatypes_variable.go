f6115e927054a4c31d5bf06974ff26ed3245bd02
Ere-yiga <erehdagr8@gmail.com>
2026-06-12 10:28:57 +0100

    data types and variables

--- datatypes_variable.go
+++ datatypes_variable.go
@@ -5,7 +5,11 @@ import "fmt"
 func main(){
 	var x string = "data types and variable"
 	var y uint8 = 255
+
+	//implicit assignment
+	z := "I am an implicit assignment"
+
 	fmt.Println(x)
 	fmt.Println(y)
-
+	fmt.Println(z)
 }
