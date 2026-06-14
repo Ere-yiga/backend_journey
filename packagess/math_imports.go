e2df5a7b7dd2c346c41f666041059f73e14969c8
Ere-yiga <erehdagr8@gmail.com>
2026-06-14 23:15:28 +0100

    looked into other go import packages

---
+++ packagess/math_imports.go
@@ -0,0 +1,34 @@
+// there are other ways you can import in Go
+
+
+package main
+
+import (
+	"fmt"
+	"math"
+
+	//conversion to string
+	"strconv"
+)
+
+func main(){
+	//square root
+	fmt.Println(math.Sqrt(9))
+
+	//exponent
+	fmt.Println(math.Pow(3, 3))
+
+	a := "1234"
+	c, err := strconv.Atoi(a)
+	fmt.Println(c, err)
+
+	//we also have parseint.
+	//it comes with somet things thoug, the value, base and bits
+
+	b := "234"
+	d, err := strconv.ParseInt(b, 10, 0)
+	fmt.Println(d)
+}
+//summary
+
+//types that go in, should be out, we can use a type conversion to use operand
